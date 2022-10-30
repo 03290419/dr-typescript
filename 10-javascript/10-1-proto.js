fb09036ed494dc4bdb570fe8b530f394e4eebffd
hyunjin <alfma123@naver.com>
2022-10-30 20:46:46 +0900

    :sparkles: prototype
    prototype

---
+++ 10-javascript/10-1-proto.js
@@ -0,0 +1,33 @@
+const x = {};
+const y = {};
+console.log(x);
+console.log(y);
+console.log(x.__proto__ === y.__proto__);
+
+const array = [];
+console.log(array);
+
+console.clear();
+function CoffeeMachine(beans) {
+  this.beans = beans;
+  //Instance member level
+  //   this.makeCoffee = (shots) => {
+  //     console.log("making.... ");
+  //   };
+}
+// prototype member level
+CoffeeMachine.prototype.makeCoffee = () => {
+  console.log("making....");
+};
+const machine1 = new CoffeeMachine(10);
+const machine2 = new CoffeeMachine(20);
+console.log(machine1);
+console.log(machine1);
+
+function LatteMachine(milk) {
+  this.milk = milk;
+}
+LatteMachine.prototype = Object.create(CoffeeMachine.prototype);
+const latteMachine = new LatteMachine(123);
+latteMachine.makeCoffee();
+console.log(latteMachine);
