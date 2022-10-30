a3de65b626e9e45f4c8b2740be352f8285e54ab4
hyunjin <alfma123@naver.com>
2022-10-30 21:22:48 +0900

    :sparkles: module
    module

---
+++ 10-javascript/10-3-module.js
@@ -0,0 +1,9 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export function print() {
+  console.log("df");
+}
+
+export const number = 1;
---
+++ 10-javascript/10-3-module2.js
@@ -0,0 +1,5 @@
+import * as md from "./10-3-module.js";
+
+console.log(md.add(1, 2));
+md.print();
+md.number;
