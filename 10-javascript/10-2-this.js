398139ba9a86de1ceeb2a4304a5daaf21be57e4f
hyunjin <alfma123@naver.com>
2022-10-30 21:13:59 +0900

    :sparkles: this
    this

---
+++ 10-javascript/10-2-this.js
@@ -0,0 +1,26 @@
+console.log(this);
+function simpleFunction() {
+  console.log(this);
+}
+window.simpleFunction();
+simpleFunction();
+console.clear();
+
+class Counter {
+  count = 0;
+  increase = () => {
+    console.log(this);
+  };
+}
+
+const counter = new Counter();
+counter.increase();
+
+// const caller = counter.increase;
+const caller = counter.increase.bind(counter);
+caller();
+
+class Bob {}
+const bob = new Bob();
+bob.run = counter.increase;
+bob.run();
