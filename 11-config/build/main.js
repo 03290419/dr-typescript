43b7807758b21de804c026b60c52cb63b19896ec
hyunjin <alfma123@naver.com>
2022-10-30 22:40:33 +0900

    :sparkles: sourceMap
    sourceMap

--- 11-config/build/main.js
+++ 11-config/build/main.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("hahas");
+var Car = /** @class */ (function () {
+    function Car() {
+        this.engine = 0;
+    }
+    Car.prototype.move = function () {
+        var engine = this.engine + 1;
+        console.log("engine");
+        console.log(engine);
+    };
+    return Car;
+}());
+var car = new Car();
+car.move();
+//# sourceMappingURL=main.js.map
--- 11-config/src/main.ts
+++ 11-config/src/main.ts
@@ -1,1 +1,11 @@
-console.log("hahas");
+class Car {
+  engine = 0;
+  move() {
+    const engine = this.engine + 1;
+    console.log("engine");
+    console.log(engine);
+  }
+}
+
+const car = new Car();
+car.move();
