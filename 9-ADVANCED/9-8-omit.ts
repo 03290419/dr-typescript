4cd926acb8f970c32839b48d10a3a0f0cdd2786a
hyunjin <alfma123@naver.com>
2022-10-30 20:22:00 +0900

    :sparkles: omit and record

---
+++ 9-ADVANCED/9-8-omit.ts
@@ -0,0 +1,23 @@
+{
+  type Video = {
+    id: string;
+    title: string;
+    url: string;
+    data: string;
+  };
+  type VideoMetadata = Omit<Video, "url" | "data">;
+  function getVideo(id: string): Video {
+    return {
+      id,
+      title: "video",
+      url: "https://..",
+      data: "byte-data..",
+    };
+  }
+  function getVideoMetadata(id: string): VideoMetadata {
+    return {
+      id: id,
+      title: "title",
+    };
+  }
+}
